83f94feffc293e32ccc747336093d89c3029b1b8
APaikens <shamanis@gmail.com>
2018-02-06 10:31:46 +0200

    Update to ZF3

--- src/GoalioForgotPassword/Options/Service/ModuleOptionsFactory.php
+++ src/GoalioForgotPassword/Options/Service/ModuleOptionsFactory.php
@@ -7,9 +7,13 @@ use Zend\ServiceManager\FactoryInterface;
 
 class ModuleOptionsFactory implements FactoryInterface {
 
-    public function createService(ServiceLocatorInterface $serviceLocator) {
-        $config = $serviceLocator->get('Config');
+    public function __invoke(\Interop\Container\ContainerInterface $container, $requestedName, array $options = NULL) {
+        $config = $container->get('Config');
         return new ModuleOptions(isset($config['goalioforgotpassword']) ? $config['goalioforgotpassword'] : array());
+    }
+
+    public function createService(ServiceLocatorInterface $serviceLocator) {
+        return $this->__invoke($serviceLocator,null);
     }
 
 }
