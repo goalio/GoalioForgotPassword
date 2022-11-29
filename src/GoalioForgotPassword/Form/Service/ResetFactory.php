00bd492a6babece96467e1f47d34a883ed7f887a
martinsDzirnieks <77049636+martinsDzirnieks@users.noreply.github.com>
2022-11-29 13:28:58 +0200

    Update ResetFactory.php

--- src/GoalioForgotPassword/Form/Service/ResetFactory.php
+++ src/GoalioForgotPassword/Form/Service/ResetFactory.php
@@ -5,13 +5,21 @@ use GoalioForgotPassword\Form\Reset;
 use GoalioForgotPassword\Form\ResetFilter;
 use Laminas\ServiceManager\ServiceLocatorInterface;
 use Laminas\ServiceManager\FactoryInterface;
+use DiviUser\Validator\PasswordIsValid;
 
 class ResetFactory implements FactoryInterface {
 
     public function __invoke(\Interop\Container\ContainerInterface $container, $requestedName, array $options = NULL) {
         $options = $container->get('goalioforgotpassword_module_options');
+        $config = $container->get('config');
         $form = new Reset(null, $options);
-        $form->setInputFilter(new ResetFilter($options));
+        $form->setInputFilter(new ResetFilter($options,
+                new PasswordIsValid(
+                    $container->get('zfcuser_module_options'),
+                    $config['zfcuser']
+                ),
+                $config['zfcuser'],                               
+             ));
         return $form;
     }
 
